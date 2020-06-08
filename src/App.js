8810b5d0aa1e0d3d2005ec9144704fa0af6f5dad
harsh <harsh710pat@gmail.com>
2020-06-08 17:38:37 +0530

    hey react routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+
+const Hatspage = () => (
+  <div>
+    <h1> Hats Page!</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      {/* <Switch> //as soon as path matches it just routes the original content */}
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={Hatspage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,8 +45,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, size,id }) => (
-          <MenuItem key={id} title={title} size = {size} imageUrl={imageUrl} />
+        {this.state.sections.map(({id, ...otherSectionProps }) => (
+          <MenuItem key={id}   {...otherSectionProps }/>
         ))}
       </div>
     );
