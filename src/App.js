6e71170c234c13c83e80cf43ab3bc8bb516ac0f4
harsh <harsh710pat@gmail.com>
2020-06-08 18:33:47 +0530

    collection preview and collection items styling complete except for add to card and Animation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
+import ShopPage from './pages/shop/shop.component'
 
-const Hatspage = () => (
-  <div>
-    <h1> Hats Page!</h1>
-  </div>
-);
+
+
+
 
 function App() {
   return (
@@ -15,7 +14,7 @@ function App() {
       {/* <Switch> //as soon as path matches it just routes the original content */}
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={Hatspage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
